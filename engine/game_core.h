049364e4a1001f9ae14fc45bb1d7bb114a3be05c
Lukaviy <jiukaviy@gmail.com>
2017-09-30 11:25:20 +1000

    Use SFML instead of clean OpenGL

--- engine/game_core.h
+++ engine/game_core.h
@@ -19,15 +19,16 @@ public:
 
 	enum State_t {
 		PLAYING,
-		PAUSED,
 		DEAD,
 	};
 
 	void step(float dtime, bool flap);
+	void reset();
 
 	Vec_t get_bird_pos() const;
 	Vec_t get_bird_velocity() const;
 	int get_current_ceil() const;
+	State_t get_state() const;
 
 	const MapElement_t* get_map();
 private:
@@ -43,13 +44,15 @@ private:
 	float _bird_height;
 	float _bird_width; 
 	float _hole_size;
+	float _start_pos;
+	State_t _state;
 	MapElement_t _map[MAP_SIZE];
 };
 
 template <int MAP_SIZE>
 GameCore_t<MAP_SIZE>::GameCore_t(float x_velocity, float y_accel, float y_flap_accel, float tube_dist, float tube_width, float hole_size, float start_pos, float bird_heiht, float bird_width)
 	: _x_pos(start_pos), _y_pos(0.5), _x_velocity(x_velocity), _y_velocity(0), _y_accel(y_accel), _y_flap_accel(y_flap_accel), _tube_dist(tube_dist),
-	_tube_width(tube_width), _curr_cell(0), _bird_height(bird_heiht), _bird_width(bird_width), _hole_size(hole_size)
+	_tube_width(tube_width), _curr_cell(0), _bird_height(bird_heiht), _bird_width(bird_width), _hole_size(hole_size), _state(PLAYING), _start_pos(start_pos)
 {
 	for (int i = 0; i < MAP_SIZE; i++) {
 		_map[i].y_pos = -0.6f + i * 1.f / MAP_SIZE * 1.8f;
@@ -60,9 +63,17 @@ GameCore_t<MAP_SIZE>::GameCore_t(float x_velocity, float y_accel, float y_flap_a
 
 template <int MAP_SIZE>
 void GameCore_t<MAP_SIZE>::step(float dtime, bool flap) {
+	if (_state == DEAD) {
+		if (_y_pos >= -1) {
+			_y_velocity += _y_accel * dtime;
+			_y_pos += _y_velocity * dtime;
+		}
+		return;
+	}
+
 	if (_map[_curr_cell % MAP_SIZE].x_pos + _tube_width / 2 <= _x_pos - _bird_width / 2) {
 		_map[(_curr_cell + MAP_SIZE - 1) % MAP_SIZE].x_pos = _map[(_curr_cell + MAP_SIZE - 2) % MAP_SIZE].x_pos + _tube_width + _tube_dist;
-		_map[(_curr_cell + MAP_SIZE - 1) % MAP_SIZE].y_pos = (rand() % 20) / 40.f;
+		_map[(_curr_cell + MAP_SIZE - 1) % MAP_SIZE].y_pos = -0.7f + (rand() % 160) / 100.f;
 		_map[(_curr_cell + MAP_SIZE - 1) % MAP_SIZE].hole_size = _hole_size + (rand() % 50) / 100.f;
 		++_curr_cell;
 	}
@@ -71,11 +82,24 @@ void GameCore_t<MAP_SIZE>::step(float dtime, bool flap) {
 		(_y_pos + _bird_height / 2 >= hole.y_pos + hole.hole_size / 2 || _y_pos - _bird_height / 2 <= hole.y_pos - hole.hole_size / 2) ||
 		_y_pos <= -1 || _y_pos >= 1))
 	{
-		return;
+		_state = DEAD;
 	}
 		_y_velocity += (_y_accel + _y_flap_accel * flap) * dtime;
 		_x_pos += _x_velocity * dtime;
 		_y_pos += _y_velocity * dtime;
+}
+
+
+template <int MAP_SIZE>
+void GameCore_t<MAP_SIZE>::reset() {
+	_x_pos = _start_pos;
+	_y_pos = 0.5;
+	_y_velocity = 0;
+	_state = PLAYING;
+	_curr_cell = 0;
+	for (int i = 0; i < MAP_SIZE; i++) {
+		_map[i].x_pos = i * (_tube_width + _tube_dist);
+	}
 }
 
 template <int MAP_SIZE>
@@ -91,6 +115,11 @@ typename GameCore_t<MAP_SIZE>::Vec_t GameCore_t<MAP_SIZE>::get_bird_velocity() c
 template <int MAP_SIZE>
 int GameCore_t<MAP_SIZE>::get_current_ceil() const {
 	return _curr_cell;
+}
+
+template <int MAP_SIZE>
+typename GameCore_t<MAP_SIZE>::State_t GameCore_t<MAP_SIZE>::get_state() const {
+	return _state;
 }
 
 template <int MAP_SIZE>
--- main.cpp
+++ main.cpp
@@ -195,7 +195,7 @@ const int map_size = 3;
 const float dist_between_tubes = 0.9f;
 const float tube_width = 0.4f;
 
-GameCore_t<map_size> game_core(0.1f, -0.05f, 0.3f, dist_between_tubes, tube_width, 0.5f, -2.f, 0.08f, 0.08f);
+GameCore_t<map_size> game_core(1.f, -4.f, 20.f, dist_between_tubes, tube_width, 0.5f, -2.f, 0.08f, 0.08f);
 
 int last_time_since_update = 0;
 void idle() {
@@ -204,6 +204,10 @@ void idle() {
 	last_time_since_update = time;
 
 	game_core.step(dtime, keys[' ']);
+
+	if (keys['r']) {
+		game_core.reset();
+	}
 
 	glutPostRedisplay();
 }
@@ -277,13 +281,9 @@ void draw() {
 
 	glDisableVertexAttribArray(attribute_coord3d);
 
-	const unsigned char text[] = "asd";
-
-	glutBitmapString(GLUT_BITMAP_9_BY_15, text);
-
 	glutSwapBuffers();
 }
-
+/*
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE | GLUT_DEPTH);
@@ -316,4 +316,100 @@ int main(int argc, char** argv) {
 	} else {
 		std::cout << "Something went wrong" << std::endl;
 	}
+}
+*/
+#include <SFML/Graphics.hpp>
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Flappy Bird");
+	sf::RectangleShape bird(sf::Vector2<float>(0.1f, 0.1f));
+	sf::RectangleShape up_tube(sf::Vector2<float>(tube_width, 2.f));
+	sf::RectangleShape bottom_tube(sf::Vector2<float>(tube_width, 2.f));
+	sf::View game_view(sf::FloatRect(-1.f, 1.f, 2.f, -2.f));
+	sf::View gui_view(sf::FloatRect(0.f, 0.f, 500.f, 500.f));
+	bird.setFillColor(sf::Color::Red);
+	up_tube.setFillColor(sf::Color::Green);
+	bottom_tube.setFillColor(sf::Color::Green);
+	bird.move(-0.05f, -0.05f);
+	up_tube.move(-tube_width / 2.f, 0.f);
+	bottom_tube.move(-tube_width / 2.f, -2.f);
+
+	sf::Font font;
+	if (!font.loadFromFile("fonts\\machine_c.otf"))
+		return EXIT_FAILURE;
+
+	window.setVerticalSyncEnabled(true);
+	window.setFramerateLimit(60);
+	window.setView(game_view);
+
+	sf::Clock deltaClock;
+	deltaClock.restart();
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		sf::Time dt = deltaClock.restart();
+		auto dtm = dt.asMilliseconds();
+
+		game_core.step(dtm / 1000.f, sf::Keyboard::isKeyPressed(sf::Keyboard::Space));
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
+			game_core.reset();
+		}
+
+		auto bird_pos = game_core.get_bird_pos();
+
+		auto bv = game_core.get_bird_velocity();
+
+		float velocity = length(glm::vec2(bv.x, bv.y));
+		float cos_b = bv.y / velocity;
+		float sin_b = bv.x / velocity;
+
+		sf::Transform t3 (
+			cos_b, sin_b, 0,
+			-sin_b, cos_b, bird_pos.y,
+			0, 0, 1
+		);
+
+		window.clear();
+
+		window.setView(game_view);
+
+		auto map = game_core.get_map();
+		int curr_ceil = game_core.get_current_ceil();
+		
+		for (int i = 0; i < map_size; i++) {
+			auto tube = map[(curr_ceil + i) % map_size];
+
+			float hole_size = tube.hole_size;
+			float hole_pos = tube.y_pos;
+			float x_pos = tube.x_pos - bird_pos.x;
+
+			sf::Transform up_tube_transform;
+			sf::Transform bottom_tube_transform;
+			up_tube_transform.translate(x_pos, hole_pos + hole_size / 2.f);
+			bottom_tube_transform.translate(x_pos, hole_pos - hole_size / 2.f);
+
+			window.draw(up_tube, up_tube_transform);
+			window.draw(bottom_tube, bottom_tube_transform);
+		}
+		
+		window.draw(bird, t3);
+
+		sf::Text text(std::to_string(game_core.get_current_ceil()), font, 50.f);
+		text.setFillColor(sf::Color::White);
+		text.setOutlineColor(sf::Color::Black);
+		text.move(250.f, 20.f);
+
+		window.setView(gui_view);
+		window.draw(text);
+
+		window.display();
+	}
+
+	return 0;
 }
