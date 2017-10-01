b58e0536d755b2cd3e93ae1a6fc56cbf7c9fbd03
Lukaviy <jiukaviy@gmail.com>
2017-10-01 16:28:08 +1000

    Scene: Make default realisation of virtual functions

--- scenes/scene.h
+++ scenes/scene.h
@@ -5,7 +5,7 @@
 
 class Scene_t : public sf::Drawable, public sf::Transformable {
 public:
-	virtual void step(float dt) = 0;
-	virtual void send_event(sf::Event event) = 0;
+	virtual void step(float dt) {}
+	virtual void send_event(sf::Event event) {}
 	virtual void reset() {};
 };
