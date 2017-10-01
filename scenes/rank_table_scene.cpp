a1de297689cf665d5c9798aa971f6457ca762660
Lukaviy <jiukaviy@gmail.com>
2017-10-01 16:28:08 +1000

    RankTable: Draw part of table, where player is placed

--- scenes/rank_table_scene.cpp
+++ scenes/rank_table_scene.cpp
@@ -7,7 +7,7 @@
 
 RankTableScene_t::RankTableScene_t(RankTable_t& rank_table) : 
 	_rank_table(rank_table), _score(0), _elapsed_time(0), _last_type_time(0), _status(START),
-	_view(sf::Rect<float>(0.f, 0.f, 500.f, 500.f)), _save_score_time(0) {}
+	_view(sf::Rect<float>(0.f, 0.f, 500.f, 500.f)), _save_score_time(0), _place(0) {}
 
 void RankTableScene_t::step(float dt) {
 	_elapsed_time += dt;
@@ -55,6 +55,7 @@ void RankTableScene_t::set_font(sf::Font font) {
 
 void RankTableScene_t::set_score(unsigned int score) {
 	_score = score;
+	_place = _rank_table.find_place(score);
 }
 
 RankTableScene_t::RankTableSceneStatus_t RankTableScene_t::get_state() const {
@@ -89,15 +90,24 @@ void RankTableScene_t::draw(sf::RenderTarget& target, sf::RenderStates states) c
 
 	auto score_text_color = sf::Color(255, 255, 255, std::min(0.5f, std::max(0.f, _elapsed_time - 0.5f)) * 2.f * 255); 
 
-	for (int i = 0; i < std::min(_rank_table.size(), size_t(10)); i++) {
-		sf::Text player_name_text(_rank_table[i].player_name, _font, 20.f);
-		player_name_text.setFillColor(score_text_color);
-		player_name_text.move(120.f, 70 + 20.f * i);
+	unsigned int start = std::max(0, signed(_place) - 5);
+	unsigned int end = std::min(_rank_table.size(), start + 10);
 
-		target.draw(player_name_text, states);
-		player_name_text.setString(std::to_string(_rank_table[i].score));
-		player_name_text.move(100.f + std::min(0.5f, std::max(0.f, _elapsed_time - 0.5f)) * 2.f * 30.f, 0);
-		target.draw(player_name_text, states);
+ 	for (int i = start; i < end; i++) {
+		sf::Text text(std::to_string(i + 1) + '.', _font, 20.f);
+		float width = text.getLocalBounds().width;
+		text.setPosition(110.f - width, 70 + 20.f * (i - start));
+		text.setFillColor(score_text_color);
+		target.draw(text, states);
+
+		text.setString(_rank_table[i].player_name);
+		text.setFillColor(score_text_color);
+		text.move(width + 15.f, 0);
+		target.draw(text, states);
+
+		text.setString(std::to_string(_rank_table[i].score));
+		text.move(100.f + std::min(0.5f, std::max(0.f, _elapsed_time - 0.5f)) * 2.f * 30.f, 0);
+		target.draw(text, states);
 	}
 
 	if (_status == SCORE_SAVED && _elapsed_time > 1.f) {
